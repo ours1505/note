4e095f37ee67a974ec6484d82e24efb6bd1cefc2
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-01-23 22:30:49 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -3,7 +3,7 @@ cd /root
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 lsmod | grep bbr
-wget https://github.com/ours1505/note/blob/main/ZBProxy-linux-amd64-v1?raw=true
+wget "https://github.com/ours1505/note/blob/main/ZBProxy-linux-amd64-v1?raw=true"
 chmod +x ZBProxy-linux-amd64-v1
 wget https://github.com/ours1505/note/raw/main/ZBProxy.json
 cat << EOF > /usr/lib/systemd/system/ZBProxy.service
