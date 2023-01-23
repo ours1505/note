6010e7c78c8c6185e982980a656fe314dabba7ea
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-01-23 22:30:18 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -3,9 +3,9 @@ cd /root
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 lsmod | grep bbr
-wget https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1
+wget https://github.com/ours1505/note/blob/main/ZBProxy-linux-amd64-v1?raw=true
 chmod +x ZBProxy-linux-amd64-v1
-wget https://raw.githubusercontent.com/ours1505/note/main/ZBProxy.json
+wget https://github.com/ours1505/note/raw/main/ZBProxy.json
 cat << EOF > /usr/lib/systemd/system/ZBProxy.service
 [Unit]
 Description=ZBProxy
@@ -21,4 +21,3 @@ EOF
 
 systemctl enable ZBProxy
 systemctl start ZBProxy
-reboot
