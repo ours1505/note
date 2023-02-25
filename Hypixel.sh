5c6c1a553f09337cb768e4d6f90f23a90e733106
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-02-25 10:20:15 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -6,10 +6,9 @@ lsmod | grep bbr
 wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
 chmod +x ZBProxy-linux-amd64-v1
 wget https://github.com/ours1505/note/raw/main/ZBProxy.json
-sed "s?NameForChange?$(curl https://api.zsshop.cf/MCName.txt)?" ZBProxy.json
 wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
 chmod +x /usr/lib/systemd/system/ZBProxy.service
-sed "s?NameForChange?$(curl https://api.zsshop.cf/MCName.txt)?" ZBProxy.json
+sed "s|NameForChange|$(curl https://api.zsshop.cf/MCName.txt)|" ZBProxy.json
 
 systemctl enable ZBProxy
 systemctl start ZBProxy
