d636ef3901073187a7699c4c8edbe1ddf20f4cb1
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-04-15 20:13:21 +0800

    Update Hypixel.sh
    update iptables rule to anti-scan

--- Hypixel.sh
+++ Hypixel.sh
@@ -8,3 +8,9 @@ chmod +x /usr/lib/systemd/system/ZBProxy.service
 sed -i "s|"NAMEFORCHANGE"|$(curl https://api.zsshop.cf/MCName.txt)|" ZBProxy.json
 systemctl enable ZBProxy
 systemctl start ZBProxy
+iptables -P INPUT DROP
+iptables -A INPUT -s 154.7.180.101 -p all -j ACCEPT
+iptables -A INPUT -s 159.75.98.102 -p all -j ACCEPT
+iptables -A INPUT -s 175.24.181.215 -p all -j ACCEPT
+iptables -A INPUT -s 16.162.93.184 -p all -j ACCEPT
+iptables -A INPUT -s 54.238.208.44 -p all -j ACCEPT
