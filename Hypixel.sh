a7a57ba26571bad33d209147c168ac7f28fc60e9
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-08 21:43:47 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -5,11 +5,11 @@ chmod +x ZBProxy-linux-amd64-v1
 wget https://github.com/ours1505/note/raw/main/ZBProxy.json
 wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
 chmod +x /usr/lib/systemd/system/ZBProxy.service
-sed -i "s|"NAMEFORCHANGE"|$(curl https://api.zsshop.cf/MCName.txt)|" ZBProxy.json
+sed -i "s|"NAMEFORCHANGE"|$(curl https://botapi.nyaproxy.xyz/MCName.txt)|" ZBProxy.json
 systemctl enable ZBProxy
 systemctl start ZBProxy
 apt update
-# apt install iptables -y
+apt install iptables -y
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -s 154.7.180.101 -p all -j ACCEPT
 iptables -A INPUT -s 159.75.98.102 -p all -j ACCEPT
@@ -18,5 +18,6 @@ iptables -A INPUT -s 16.162.93.184 -p all -j ACCEPT
 iptables -A INPUT -s 54.238.208.44 -p all -j ACCEPT
 iptables -A INPUT -s 103.170.233.181 -p all -j ACCEPT
 iptables -A INPUT -s 103.170.233.0/24 -p all -j ACCEPT
+iptables -A INPUT -s 154.23.240.188 -p all -j ACCEPT
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
