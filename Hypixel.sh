d59bc0beb91e8b6d1c1e322b6a38fb11c694e698
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-09 21:35:06 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo root:@NyaProxy |sudo chpasswd root
+echo root:NyaProxy1982822398 |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo systemctl restart sshd
