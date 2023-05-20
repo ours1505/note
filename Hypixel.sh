cd592f122971ffd6560e4ac7f5336ecfc90aab5f
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-20 23:09:52 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -3,7 +3,7 @@ echo root:NyaProxyDotXyz |sudo chpasswd root
 cd /root
 wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
 chmod +x ZBProxy-linux-amd64-v1
-wget https://github.com/ours1505/note/raw/main/ZBProxy.json
+wget -O ZBProxy.json https://github.com/ours1505/note/raw/main/hypixel.json
 wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
 chmod +x /usr/lib/systemd/system/ZBProxy.service
 sed -i "s|"NAMEFORCHANGE"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" ZBProxy.json
