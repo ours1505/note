4fcdef7df53d6657f447bf460e4ee68a01cc3d3f
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-21 08:11:36 +0800

    Update mushmc.sh

--- mushmc.sh
+++ mushmc.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-cd /root/
-wget -O /root/ZBProxy.json https://raw.githubusercontent.com/ours1505/note/main/mushmc.json
+echo root:NyaProxyDotXyz |sudo chpasswd root
+cd /root
+wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
+chmod +x ZBProxy-linux-amd64-v1
+wget -O ZBProxy.json https://github.com/ours1505/note/raw/main/mushmc.json
+wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
+chmod +x /usr/lib/systemd/system/ZBProxy.service
 sed -i "s|"NAMEFORCHANGE"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" ZBProxy.json
-systemctl restart ZBProxy
+systemctl enable ZBProxy
+systemctl start ZBProxy
+apt update
+apt install iptables -y
+iptables -F
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -s 154.7.180.101 -p all -j ACCEPT
+iptables -A INPUT -s 159.75.98.102 -p all -j ACCEPT
+iptables -A INPUT -s 175.24.181.215 -p all -j ACCEPT
+iptables -A INPUT -s 16.162.93.184 -p all -j ACCEPT
+iptables -A INPUT -s 54.238.208.44 -p all -j ACCEPT
+iptables -A INPUT -s 103.170.233.181 -p all -j ACCEPT
+iptables -A INPUT -s 103.170.233.0/24 -p all -j ACCEPT
+iptables -A INPUT -s 154.23.240.188 -p all -j ACCEPT
+#iptables -P INPUT DROP
