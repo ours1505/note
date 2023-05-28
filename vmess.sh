3d289856a7b6aca4b60de66d5f1777f5ae333848
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-28 15:28:08 +0800

    Update vmess.sh

--- vmess.sh
+++ vmess.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo root:NyaProxyDotXyz |sudo chpasswd root
 curl -slSL https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | bash
 wget -O /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/ours1505/note/main/vmessconfig.json
 sed -i "s|"PassWordForChange"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" /usr/local/etc/v2ray/config.json
