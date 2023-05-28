e9c51a114789a820a58d89761e72a482732eb049
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-05-28 13:31:41 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -4,5 +4,7 @@ wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.3/s
 tar -xvJf shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
 wget -O /root/config.json https://raw.githubusercontent.com/ours1505/note/main/ssconfig.json
 sed -i "s|"PassWordToChange"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" config.json
+wget -O /usr/lib/systemd/system/ssserver.service https://raw.githubusercontent.com/ours1505/note/main/ssserver.service
+chmod +x /usr/lib/systemd/system/ssserver.service
 systemctl enable ssserver
 systemctl start ssserver
