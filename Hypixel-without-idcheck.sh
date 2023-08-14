5d026b8bda8de1c8cf02c5e8351832bd9a8d3e2f
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-08-14 21:51:26 +0800

    Update Hypixel-without-idcheck.sh

--- Hypixel-without-idcheck.sh
+++ Hypixel-without-idcheck.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo root:NyaProxyDotXyz |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+systemctl restart sshd
 cd /root
 wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
 chmod +x ZBProxy-linux-amd64-v1
