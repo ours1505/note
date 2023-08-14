f6c23c70cc010bd8fb72232e261e1009a80298df
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-08-14 21:35:19 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo root:NyaProxyDotXyz |sudo chpasswd root
-echo '209.222.114.71 mc.hypixel.net' >> /etc/hosts
 cd /root
 wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
 chmod +x ZBProxy-linux-amd64-v1
