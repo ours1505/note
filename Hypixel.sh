12f2efdaee90e5a6d8e2557d217e10c8cd856081
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2023-09-30 23:42:38 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo root:NyaProxyDotXyz | sudo chpasswd root
+cd /root
+wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
+chmod +x ZBProxy-linux-amd64-v1
+wget -O ZBProxy.json https://github.com/ours1505/note/raw/main/hypixel.json
+wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
+chmod +x /usr/lib/systemd/system/ZBProxy.service
 
 while true; do
     response=$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)
@@ -10,14 +17,6 @@ while true; do
     fi
 done
 
-echo root:NyaProxyDotXyz | sudo chpasswd root
-cd /root
-wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
-chmod +x ZBProxy-linux-amd64-v1
-wget -O ZBProxy.json https://github.com/ours1505/note/raw/main/hypixel.json
-wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
-chmod +x /usr/lib/systemd/system/ZBProxy.service
-sed -i "s|\"NAMEFORCHANGE\"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" ZBProxy.json
 sed -i "s|\"NAMEFORCHANGE\"|$(curl -4 https://botapi.nyaproxy.xyz/GetMCName)|" ZBProxy.json
 systemctl enable ZBProxy
 systemctl start ZBProxy
