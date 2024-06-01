a3a8eb1cc42d04faa90ad11ed0a9e650106c05eb
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2024-06-01 12:51:23 +0800

    Update Hypixel.sh

--- Hypixel.sh
+++ Hypixel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo root:NyaProxyDotXyz | sudo chpasswd root
 cd /root
-wget "https://raw.githubusercontent.com/ours1505/note/master/ZBProxy-linux-amd64-v1"
+wget "https://cdn.jsdelivr.net/gh/ours1505/note@raw/main/ZBProxy-linux-amd64-v1"
 chmod +x ZBProxy-linux-amd64-v1
-wget -O ZBProxy.json https://github.com/ours1505/note/raw/main/hypixel.json
-wget -O /usr/lib/systemd/system/ZBProxy.service https://github.com/ours1505/note/raw/main/ZBProxy.service
+wget -O ZBProxy.json https://cdn.jsdelivr.net/gh/ours1505/note@raw/main/hypixel.json
+wget -O /usr/lib/systemd/system/ZBProxy.service https://cdn.jsdelivr.net/gh/ours1505/note@raw/main/ZBProxy.service
 chmod +x /usr/lib/systemd/system/ZBProxy.service
 
 while true; do
